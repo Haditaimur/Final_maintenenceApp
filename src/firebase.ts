225a34c263472c60036635c913ad74b66b478ca3
Haditaimur <124944538+Haditaimur@users.noreply.github.com>
2025-12-05 02:00:26 +0000

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_AUTH_DOMAIN_HERE",
+  projectId: "YOUR_PROJECT_ID_HERE",
+  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "YOUR_SENDER_ID_HERE",
+  appId: "YOUR_APP_ID_HERE",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
