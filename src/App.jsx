5cbe27d5f5c47086359a5abebd14d82ba3a2a70e
Haditaimur <124944538+Haditaimur@users.noreply.github.com>
2025-12-05 03:45:07 +0000

    Update App.jsx
    02 changed the complete file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { subscribeToJobs, createJobInDb, updateJobInDb, deleteJobInDb } from './jobsService'
+import { subscribeToJobs, createJobInDb, updateJobInDb, deleteJobInDb } from './jobsService.js'
 
 //Data
 const initialRooms = [
